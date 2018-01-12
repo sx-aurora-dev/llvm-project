409a3e817abb5aecef89ffc145c9bcabeadec79e
Manoel Roemmer <roemmer@itc.rwth-aachen.de>
2018-01-12 17:34:46 +0100

    Add build-wrapper and add install targets for sotoc and build-wrapper

---
+++ clang/necaurora-ofld-cc1-wrapper.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+
+  if (argc < 2)
+  {
+    std::cerr << "Needs at least one argument\n";
+    return EXIT_FAILURE;
+  }
+
+  char *compiler_path = std::getenv("NECAURORA_OFLD_COMPILER");
+
+  std::string inputName(argv[1]);
+  std::stringstream additionalArgsStream;
+
+
+  for (int i = 2; i < argc; ++i) {
+    additionalArgsStream << " " << argv[i];
+  }
+  
+  std::stringstream cmdLine;
+  cmdLine << "sotoc " << inputName
+          << " -- -fopenmp "
+          << " | ";
+          
+  if (compiler_path) {
+    cmdLine << compiler_path;
+  } else {
+    cmdLine << "gcc";
+  }
+
+  cmdLine << " -x c " << additionalArgsStream.str()
+          << " -";
+  return system(cmdLine.str().c_str());
+}
---
+++ clang/necaurora-ofld-tool-wrapper.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+
+  if (argc < 2) {
+    std::cerr << "Needs at least one argument" << std::endl;
+  }
+
+  char *tool_path = std::getenv("NECAURORA_OFLD_COMPILER");
+
+  std::stringstream cmdLine;
+
+  if (tool_path) {
+    cmdLine << tool_path;
+  } else {
+    cmdLine << "ncc";
+  }
+
+  for (int i = 1; i < argc; ++i) {
+    cmdLine << " " << argv[i];
+  }
+
+  return std::system(cmdLine.str().c_str());
+}
