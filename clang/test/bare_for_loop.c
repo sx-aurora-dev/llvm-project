6ad232d6c7e8314b93e205893b3af3d6779e8932
Manoel Roemmer <roemmer@itc.rwth-aachen.de>
2018-04-04 11:18:25 +0200

    Add some additional simple test cases
    most of them are failing right now with clang as a backend (pls check
    with ncc)

---
+++ clang/test/bare_for_loop.c
@@ -0,0 +1,15 @@
+// RUN: %sotoc-transform-compile
+
+int main(void) {
+  int h = 0;
+  int i = 0;
+
+  #pragma omp target device(0)
+  #pragma omp parallel for
+  for (i = 0; i < 10; ++i) {
+    h += 1;
+  }
+
+
+  return 0;
+}
