5c4a2209ea0a95de91cecf019205a787f4cab866
Manoel Roemmer <roemmer@itc.rwth-aachen.de>
2018-04-06 13:35:43 +0200

    Fix sotoc tests

--- clang/test/declare_target_function.c
+++ clang/test/declare_target_function.c
@@ -6,14 +6,14 @@ int add_one_to_var(int var) {
   return var + 1;
 }
 
-#pragma omp declare target end
+#pragma omp end declare target
 
 int main(void) {
   int h = 0;
 
   #pragma omp target device(0)
   {
-    h = add_one_to_var(h)
+    h = add_one_to_var(h);
   }
 
 
--- clang/test/declare_target_function_moved_body.c
+++ clang/test/declare_target_function_moved_body.c
@@ -4,14 +4,14 @@
 
 int add_one_to_var(int var);
 
-#pragma omp declare target end
+#pragma omp end declare target
 
 int main(void) {
   int h = 0;
 
   #pragma omp target device(0)
   {
-    h = add_one_to_var(h)
+    h = add_one_to_var(h);
   }
 
 
