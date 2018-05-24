17b7276595d857863c92c02c2662c7b769520c0b
Kazuhisa Ishizaka <k-ishizaka@ay.jp.nec.com>
2018-05-24 18:13:24 +0900

    update

--- clang/lib/Headers/veintrin.h
+++ clang/lib/Headers/veintrin.h
@@ -3,7 +3,10 @@
 
 typedef double __vr __attribute__((__vector_size__(2048)));
 typedef double __vm __attribute__((__vector_size__(32)));
+typedef double __vm256 __attribute__((__vector_size__(32)));
+typedef double __vm512 __attribute__((__vector_size__(64)));
 
+// FIXME: rename to VECC_*
 enum CondCodes {
     CC_AF    =  0   ,  // Never
     CC_G     =  1   ,  // Greater
@@ -23,6 +26,18 @@ enum CondCodes {
     CC_AT    = 15   ,  // Always
 };
 
+/*
+ * suffix
+ *
+ * v: vector register
+ * s: scalar register
+ * I: immediate
+ * m: mask register
+ * M: 512b mask register (paired vm)
+ * c: condition code
+ * i: RFU (vector index register)
+ */
+
 #define _ve_lvl(l) __builtin_ve_lvl(l)
 #define _ve_vld(p, s) __builtin_ve_vld(p, s)
 #define _ve_vldu(p, s) __builtin_ve_vldu(p, s)
@@ -31,7 +46,13 @@ enum CondCodes {
 #define _ve_vstu(p, v, s) __builtin_ve_vstu(p, v, s)
 #define _ve_vstl(p, v, s) __builtin_ve_vstl(p, v, s)
 
-#define _ve_lsv_vss __builtin_ve_lsv_vss
+#define _ve_pack_f32p __builtin_ve_pack_f32p
+
+#define _ve_vec_call __builtin_ve_vec_call
+
+extern void __vec_expf(void);
+#define _ve_vec_expf_vv(vr) _ve_vec_call(vr, __vec_expf)
+
 
 #include <veintrin2.h>
 
