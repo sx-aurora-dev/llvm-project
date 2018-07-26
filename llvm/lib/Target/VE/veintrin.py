18a0e7ce43d1170af1e8fcef41aa679a241f45b3
Kazuhisa Ishizaka <k-ishizaka@ay.jp.nec.com>
2018-07-26 09:40:29 +0900

    fix intrinsic manual for VFMK

--- llvm/lib/Target/VE/veintrin.py
+++ llvm/lib/Target/VE/veintrin.py
@@ -1172,16 +1172,16 @@ T.InstX(0xBC, "VSHF", "vshf", [[VX(T_u64), VY(T_u64), VZ(T_u64), SY(T_u64)], [VX
 T.InstX(0x8D, "VCP", "vcp", [[VX(T_u64), VZ(T_u64), VM, VD(T_u64)]]).noTest()
 T.InstX(0x9D, "VEX", "vex", [[VX(T_u64), VZ(T_u64), VM, VD(T_u64)]]).noTest()
 T.VFMKm(0xB4, "VFMK", "vfmk.l")
-T.VFMKm(0xB4, "VFMS", "vfmk.w")
-T.VFMKm(0xB4, "VFMFd", "vfmk.d")
-T.VFMKm(0xB4, "VFMFs", "vfmk.s")
 T.InstX(0xB4, "VFMKat", "vfmk.at", [[VM]]).noTest()
 T.InstX(0xB4, "VFMKaf", "vfmk.af", [[VM]]).noTest()
-T.InstX(0x00, "VFMSp", "pvfmk.w", [[VM512, CCOp, VZ(T_i32)]], None, True).noTest() # Pseudo
-T.InstX(0x00, "VFMFp", "pvfmk.s", [[VM512, CCOp, VZ(T_f32)]], None, True).noTest() # Pseudo
-
 T.InstX(0x00, "VFMKpat", "pvfmk.at", [[VM512]], None, True).noTest() # Pseudo
 T.InstX(0x00, "VFMKpaf", "pvfmk.af", [[VM512]], None, True).noTest() # Pseudo
+T.VFMKm(0xB4, "VFMS", "vfmk.w")
+T.InstX(0x00, "VFMSp", "pvfmk.w", [[VM512, CCOp, VZ(T_i32)]], None, True).noTest() # Pseudo
+T.VFMKm(0xB4, "VFMFd", "vfmk.d")
+T.VFMKm(0xB4, "VFMFs", "vfmk.s")
+T.InstX(0x00, "VFMFp", "pvfmk.s", [[VM512, CCOp, VZ(T_f32)]], None, True).noTest() # Pseudo
+
 
 T.Section("5.3.2.13. Vector Recursive Relation Instructions", 32)
 T.InstX(0xEA, "VSUMSsx", "vsum.w.sx", [[VX(T_i32), VY(T_i32)]])
