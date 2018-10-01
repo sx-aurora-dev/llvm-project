8f4d2dc05459b2095d5666d86cdc8309031ea5b4
Boris Kosmynin <kosmynin@itc.rwth-aachen.de>
2018-10-01 11:40:23 +0200

    Added new testcases and modified target_teams_params case

---
+++ clang/tools/sotoc/test/tasking/taskwait.c
@@ -0,0 +1,34 @@
+// RUN: %sotoc-transform-compile
+
+
+int task(int n) {
+  if (n < 15) return 0;
+
+  #pragma omp task
+  task(n--);
+
+  return 0;
+
+}
+
+int main(void) {
+
+  #pragma omp target device(0)
+  #pragma omp parallel num_threads(10)
+  {
+    #pragma omp single
+    {
+      #pragma omp task
+      {
+        task(10);
+      }
+
+      #pragma omp task
+      {
+        task(20);
+      }
+
+      #pragma omp taskwait
+    }
+  }
+}
