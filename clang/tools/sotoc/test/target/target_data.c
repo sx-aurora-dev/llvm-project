c9006afa93e372ae83ef32295fb35f073e06e348
Boris Kosmynin <kosmynin@itc.rwth-aachen.de>
2018-10-19 10:57:04 +0200

    Added tests for target data environment directives

---
+++ clang/tools/sotoc/test/target/target_data.c
@@ -0,0 +1,26 @@
+// RUN: %sotoc-transform-compile
+// RUN: %run-on-host | FileCheck %s
+
+#include <stdio.h>
+
+int main(void) {
+  int h = 0, j = 1, i = 1;
+
+  #pragma omp target data device(0) map(to: j,i) map(from: h)
+  {
+    #pragma omp target device(0) map(to: j,i) map(from: h)
+    {
+      h = j + i;
+    }
+    #pragma omp target device(0) map(to: j,i) map(tofrom: h)
+    {
+      h += j + i;
+    }
+  }
+
+  printf("%d\n", h);
+
+  return 0;
+}
+
+// CHECK: 4
