f86ea5b7c7dcb44006d8b32f5dd85393d5188505
Tim Cramer <cramer@itc.rwth-aachen.de>
2019-02-05 16:26:21 +0100

    Fix another data race in test case.

--- clang/tools/sotoc/test/omp_statements/parallel_for_simd_stat_dimx.c
+++ clang/tools/sotoc/test/omp_statements/parallel_for_simd_stat_dimx.c
@@ -24,7 +24,7 @@ int main(void) {
     }
   }
 
-  #pragma omp parallel for simd
+  #pragma omp parallel for simd private(j,k)
   for (i = 0; i < 2; i++) {
     for (j = 0; j < 5; j++) {
       for (k = 0; k < 10; k++) {
