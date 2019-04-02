cb84983700fcb838760ad8fc95f2943c3589faaf
Manoel Roemmer <manoel.roemmer@rwth-aachen.de>
2019-04-02 15:04:15 +0200

    Add documentation to OmpPragma.*

--- clang/tools/sotoc/src/OmpPragma.h
+++ clang/tools/sotoc/src/OmpPragma.h
@@ -16,6 +16,20 @@
 
 #include "TargetCodeFragment.h"
 
+/// A helper class to rewrite some "pragma omp" (mostly teams and similar
+/// combined constructs), which are not supported by sotoc.
+/// We currently only support one team to be run on the target because ncc does
+/// not support 'freestanding' teams. So we need to remove teams and
+/// distribute constructs from the generated target code. But teams constructs
+/// can also appear in combined constructs. These combined constructs cannot
+/// simply be removed, they must be replace by "non-team" equivalents to
+/// preserve correctness.
+/// This class provides helper functions that finds a suitable replacement for
+/// omp pragmas that contain teams constructs.
+/// It is used during code generation: The omp pragma of each target region
+/// that is declared as part of a combined construct and each pragma found
+/// during pretty printing is encapsulated by an object of this class which is
+/// then used to generate a replacement.
 class OmpPragma {
   clang::PrintingPolicy PP;
   llvm::ArrayRef<clang::OMPClause *> Clauses;
@@ -28,7 +42,10 @@ public:
   OmpPragma(clang::OMPExecutableDirective *Directive, clang::PrintingPolicy PP)
       : PP(PP), Clauses(Directive->clauses()),
         Kind(Directive->getDirectiveKind()){};
+  /// Returns true if the omp pragma encapsulated, needs to be followed by a
+  /// structured block (i.e. {...}).
   bool needsStructuredBlock();
+  /// Prints a replacement omp pragma for the encapsulated pragma onto \p Out.
   void printReplacement(llvm::raw_ostream &Out);
 
 private:
