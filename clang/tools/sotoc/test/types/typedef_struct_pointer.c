68eec5bf9d5f7b36e037405beb61babc9a4d516c
Manoel Roemmer <manoel.roemmer@rwth-aachen.de>
2019-06-04 18:26:01 +0200

    Fix issue with sotoc not finding some types
    If the type is referenced only by a pointer to it, the it was not
    discovered by sotoc.

---
+++ clang/tools/sotoc/test/types/typedef_struct_pointer.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+} node_t;
+
+
+int main() {
+  node_t *list = malloc(sizeof(node_t));
+  node_t *current = list;
+  current->next = NULL;
+  current->data = 0;
+  #pragma omp target enter data map(to:current[0:1])
+  #pragma omp target
+  {
+    current->data += 1;
+  }
+
+}
