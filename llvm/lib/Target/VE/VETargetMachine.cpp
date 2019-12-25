67c54809f1f069b18a0999aaae4d7e7a09b0b8aa
Kazushi (Jam) Marukawa <marukawa@nec.com>
2019-12-25 09:56:30 +0900

    Correct VE's stack alignment from 64 bits (8 bytes) to 128 bits (16 bytes).

--- llvm/lib/Target/VE/VETargetMachine.cpp
+++ llvm/lib/Target/VE/VETargetMachine.cpp
@@ -38,8 +38,8 @@ static std::string computeDataLayout(const Triple &T) {
   // VE supports 32 bit and 64 bits integer on registers
   Ret += "-n32:64";
 
-  // Stack alignment is 64 bits
-  Ret += "-S64";
+  // Stack alignment is 128 bits
+  Ret += "-S128";
 
   // Vector alignments are 64 bits
   // Need to define all of them.  Otherwise, each alignment becomes
