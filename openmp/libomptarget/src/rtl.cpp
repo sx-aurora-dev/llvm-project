567615704ef97484eeef4be5f025ac68b810ec60
Manoel Roemmer <manoel.roemmer@rwth-aachen.de>
2020-02-19 17:36:32 +0100

    Fix name of VE plugin for plugin selection

--- openmp/libomptarget/src/rtl.cpp
+++ openmp/libomptarget/src/rtl.cpp
@@ -23,7 +23,7 @@
 
 // List of all plugins that can support offloading.
 static const char *RTLNames[] = {
-    /* Aurora target  */ "libomptarget.rtl.necaurora.so",
+    /* SX-Aurora VE target  */ "libomptarget.rtl.ve.so",
     /* PowerPC target */ "libomptarget.rtl.ppc64.so",
     /* x86_64 target  */ "libomptarget.rtl.x86_64.so",
     /* CUDA target    */ "libomptarget.rtl.cuda.so",
