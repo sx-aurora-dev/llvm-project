4c3a387c3d72fef5f3f317bba0f358a4fc3291b9
Simon Moll <simon.moll@emea.nec.com>
2020-02-20 18:07:53 +0100

    [ve][vvp] cleanup, expand uint_to/from_fp (otw breaks in promotion)

--- llvm/lib/Target/VE/VEISelLowering.cpp
+++ llvm/lib/Target/VE/VEISelLowering.cpp
@@ -1896,8 +1896,8 @@ VETargetLowering::VETargetLowering(const TargetMachine &TM,
 
   /// Conversion {
   // VE doesn't have instructions for fp<->uint, so expand them by llvm
-  setOperationAction(ISD::FP_TO_UINT, MVT::i32, Promote); // use i64
-  setOperationAction(ISD::UINT_TO_FP, MVT::i32, Promote); // use i64
+  setOperationAction(ISD::FP_TO_UINT, MVT::i32, Expand); // "Promote" actually breaks due to the v4i32 type and resulting type mismatches
+  setOperationAction(ISD::UINT_TO_FP, MVT::i32, Expand);
   setOperationAction(ISD::FP_TO_UINT, MVT::i64, Expand);
   setOperationAction(ISD::UINT_TO_FP, MVT::i64, Expand);
 
