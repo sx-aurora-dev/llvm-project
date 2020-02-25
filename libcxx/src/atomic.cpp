30025fe5050f5750e00fbefb727da1879ceb1e23
Kazushi (Jam) Marukawa <marukawa@nec.com>
2020-02-25 16:50:27 +0900

    [VE] Define several FUTEX values
    VE support futex system call partially, therefore VE doesn't
    provide linux/futex.h.  So, we modify atomic.cpp to contain
    predefined FUTEX values instead of reading them from futex.h
    for the case of VE.

--- libcxx/src/atomic.cpp
+++ libcxx/src/atomic.cpp
@@ -18,7 +18,17 @@
 #ifdef __linux__
 
 #include <unistd.h>
+#if defined(__ve__)
+// VE doesn't have linux/futex.h since futex system call is partially
+// implemented.
+#  define FUTEX_WAIT              0
+#  define FUTEX_WAKE              1
+#  define FUTEX_PRIVATE_FLAG      128
+#  define FUTEX_WAIT_PRIVATE      (FUTEX_WAIT | FUTEX_PRIVATE_FLAG)
+#  define FUTEX_WAKE_PRIVATE      (FUTEX_WAKE | FUTEX_PRIVATE_FLAG)
+#else
 #include <linux/futex.h>
+#endif
 #include <sys/syscall.h>
 
 #else // <- Add other operating systems here
