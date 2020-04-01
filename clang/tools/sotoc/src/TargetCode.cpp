90b2b06c240ff9dc416368f385c2a191ccfe8d6a
Simon Moll <simon.moll@emea.nec.com>
2020-04-01 16:27:05 +0200

    [sotoc] missing includes

--- clang/tools/sotoc/src/TargetCode.cpp
+++ clang/tools/sotoc/src/TargetCode.cpp
@@ -27,6 +27,7 @@
 #include "llvm/ADT/APInt.h"
 #include "llvm/Support/Format.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/FileSystem.h"
 
 #include "Debug.h"
 #include "OmpPragma.h"
