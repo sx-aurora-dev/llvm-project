182be326b7b3f23f3e91a5d6e40048ef190bfa21
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2020-07-23 22:45:35 +0300

    [LDA] Set up the LoopDependenceAnalysis

--- llvm/lib/Analysis/LoopDependenceAnalysis.cpp
+++ llvm/lib/Analysis/LoopDependenceAnalysis.cpp
@@ -6,12 +6,17 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// The implementation for the loop memory dependence that was originally
-// developed for the loop vectorizer.
+// The implementation of outer-loop vectorization legality analysis for
+// for the Region Vectorizer.
 //
 //===----------------------------------------------------------------------===//
 
-#include <llvm/Analysis/LoopDependenceAnalysis.h>
+#include "llvm/Analysis/LoopDependenceAnalysis.h"
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/Analysis/AliasSetTracker.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/VectorUtils.h"
+#include "llvm/IR/Instructions.h"
 
 using namespace llvm;
 
@@ -21,10 +26,81 @@ AnalysisKey LoopDependenceAnalysis::Key;
 
 LoopDependenceInfo LoopDependenceAnalysis::run(Function &F,
                                                FunctionAnalysisManager &FAM) {
-  return LoopDependenceInfo(F, FAM);
+  ScalarEvolution &SE = FAM.getResult<ScalarEvolutionAnalysis>(F);
+  TargetLibraryInfo &TLI = FAM.getResult<TargetLibraryAnalysis>(F);
+  AAResults &AA = FAM.getResult<AAManager>(F);
+  DominatorTree &DT = FAM.getResult<DominatorTreeAnalysis>(F);
+  LoopInfo &LI = FAM.getResult<LoopAnalysis>(F);
+  return LoopDependenceInfo(F, SE, TLI, AA, DT, LI);
 }
 
-LoopDependenceInfo::LoopDependenceInfo(Function &F,
-                                       FunctionAnalysisManager &FAM) {
-  abort(); // TODO do stuff
+LoopDependenceInfo::LoopDependenceInfo(Function &F, ScalarEvolution &SE,
+                                       TargetLibraryInfo &TLI, AAResults &AA,
+                                       DominatorTree &DT, LoopInfo &LI)
+    : SE(SE), TLI(TLI), AA(AA), DT(DT), LI(LI) {
+
+    dbgs() << "\n\n\nTHAT THING FINALLY WORKED\n\n\n";
+}
+
+const LoopDependence LoopDependenceInfo::getDependenceInfo(Loop &L) {
+  LoopDependence foo;
+  if (L.isAnnotatedParallel())
+    return foo;
+
+  SmallVector<LoadInst *, 16> Loads;
+  SmallVector<StoreInst *, 16> Stores;
+
+  // The AST tracks _maximal_ sets of pointers
+  // that may alias with each other.
+  AliasSetTracker AST(AA);
+
+  // Find if there's any illegal instruction and gather
+  // loads and stores. Also put the pointers in the AST.
+  for (BasicBlock *BB : L.blocks()) {
+    for (Instruction &I : *BB) {
+      if (auto *Call = dyn_cast<CallBase>(&I)) {
+        if (Call->isConvergent())
+          return foo;
+      }
+
+      // If this instruction may read from memory and it is not
+      // a simple load or a known call, we can't vectorize it.
+      if (I.mayReadFromMemory()) {
+        // Many math library functions read the rounding mode. We will only
+        // vectorize a loop if it contains known function calls that don't set
+        // the flag. Therefore, it is safe to ignore this read from memory.
+        auto *Call = dyn_cast<CallInst>(&I);
+        if (Call && getVectorIntrinsicIDForCall(Call, &TLI))
+          continue;
+
+        // If the function has an explicit vectorized counterpart, we can safely
+        // assume that it can be vectorized.
+        if (Call && !Call->isNoBuiltin() && Call->getCalledFunction() &&
+            !VFDatabase::getMappings(*Call).empty())
+          continue;
+
+        auto *Ld = dyn_cast<LoadInst>(&I);
+        if (!Ld || !Ld->isSimple())
+          return foo;
+
+        Loads.push_back(Ld);
+        AST.add(Ld);
+
+        // If this instruction may write to memory and it is not a simple store,
+        // then we can't vectorize it.
+      } else if (I.mayWriteToMemory()) {
+        auto *St = dyn_cast<StoreInst>(&I);
+        if (!St || !St->isSimple())
+          return foo;
+
+        Stores.push_back(St);
+        AST.add(St);
+      } // else -> We don't care about any other instruction.
+    }   // Next instr.
+  }     // Next block.
+
+  dbgs() << "Dumping the Alias Set Tracker\n";
+  AST.dump();
+
+
 }
