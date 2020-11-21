5b2698953e6376dcc13b22057afad6dc61f1dc49
Kazushi (Jam) Marukawa <marukawa@nec.com>
2020-11-21 10:30:08 +0900

    [VE] Update br_jt regression test

--- llvm/test/CodeGen/VE/c/br_jt.c
+++ llvm/test/CodeGen/VE/c/br_jt.c
@@ -1,10 +1,99 @@
-int br_jt(int a)
+int br_jt3(int a)
+{
+  switch (a) {
+  case 1: return a+2;
+  case 2: return a-2;
+  case 4: return a+3;
+  default: return a;
+  }
+}
+
+int br_jt4(int a)
 {
   switch (a) {
   case 1: return a+2;
   case 2: return a-2;
   case 3: return a+1;
   case 4: return a+3;
+  default: return a;
+  }
+}
+
+int br_jt7(int a)
+{
+  switch (a) {
+  case 1: return a+2;
+  case 2: return a-2;
+  case 3: return a+1;
+  case 4: return a+3;
+  case 7: return a-2;
+  case 9: return a+1;
+  case 8: return a+3;
+  default: return a;
+  }
+}
+
+int br_jt8(int a)
+{
+  switch (a) {
+  case 1: return a+2;
+  case 2: return a-2;
+  case 3: return a+1;
+  case 4: return a+3;
+  case 6: return a-5;
+  case 7: return a-2;
+  case 9: return a+1;
+  case 8: return a+3;
+  default: return a;
+  }
+}
+
+int br_jt3_m(int a, int b)
+{
+  switch (a) {
+  case 1: return a+2;
+  case 2: return a-2;
+  case 4: return b+3;
+  default: return a;
+  }
+}
+
+int br_jt4_m(int a, int b)
+{
+  switch (a) {
+  case 1: return a+2;
+  case 2: return a-2;
+  case 3: return a+1;
+  case 4: return b+3;
+  default: return a;
+  }
+}
+
+int br_jt7_m(int a, int b)
+{
+  switch (a) {
+  case 1: return a+2;
+  case 2: return a-2;
+  case 3: return a+1;
+  case 4: return b+3;
+  case 7: return b-2;
+  case 9: return a+1;
+  case 8: return a+3;
+  default: return a;
+  }
+}
+
+int br_jt8_m(int a, int b)
+{
+  switch (a) {
+  case 1: return a+2;
+  case 2: return a-2;
+  case 3: return a+1;
+  case 4: return b+3;
+  case 6: return b-5;
+  case 7: return b-2;
+  case 9: return a+1;
+  case 8: return a+3;
   default: return a;
   }
 }
