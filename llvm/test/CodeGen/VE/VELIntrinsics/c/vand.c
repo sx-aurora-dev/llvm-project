fc38286b2bef145a500e9b1b69d90f68e135c32a
tkohara <tkohara-tgl@ncontr.com>
2020-11-30 10:49:25 +0900

    [VE] Add a regression test of vand intrinsic instructions

---
+++ llvm/test/CodeGen/VE/VELIntrinsics/c/vand.c
@@ -0,0 +1,66 @@
+#include "types.h"
+#include "velintrin.h"
+
+#define REGCALL fastcall
+
+/// Test vector and intrinsic instructions
+///
+/// Note:
+///   We test VAND*vvl, VAND*vvl_v, VAND*rvl, VAND*rvl_v, VAND*vvml_v,
+///   VAND*rvml_v, PVAND*vvl, PVAND*vvl_v, PVAND*rvl, PVAND*rvl_v, PVAND*vvml_v, and
+///   PVAND*rvml_v instructions.
+
+#define VAND_TEST(INST, TYPE) \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvl(__vr l, __vr r) { \
+  return _vel_ ## INST ## _vvvl(l, r, 256); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvvl(__vr l, __vr r, __vr b) { \
+  return _vel_ ## INST ## _vvvvl(l, r, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvl(TYPE l, __vr r) { \
+  return _vel_ ## INST ## _vsvl(l, r, 256); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvvl(TYPE l, __vr r, __vr b) { \
+  return _vel_ ## INST ## _vsvvl(l, r, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvmvl(__vr l, __vr r, __vm256 m, __vr b) { \
+  return _vel_ ## INST ## _vvvmvl(l, r, m, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvmvl(TYPE l, __vr r, __vm256 m, __vr b) { \
+  return _vel_ ## INST ## _vsvmvl(l, r, m, b, 128); \
+}
+
+#define PVAND_TEST(INST, TYPE) \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvl(__vr l, __vr r) { \
+  return _vel_ ## INST ## _vvvl(l, r, 256); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvvl(__vr l, __vr r, __vr b) { \
+  return _vel_ ## INST ## _vvvvl(l, r, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvl(TYPE l, __vr r) { \
+  return _vel_ ## INST ## _vsvl(l, r, 256); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvvl(TYPE l, __vr r, __vr b) { \
+  return _vel_ ## INST ## _vsvvl(l, r, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvMvl(__vr l, __vr r, __vm512 m, __vr b) { \
+  return _vel_ ## INST ## _vvvMvl(l, r, m, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvMvl(TYPE l, __vr r, __vm512 m, __vr b) { \
+  return _vel_ ## INST ## _vsvMvl(l, r, m, b, 128); \
+}
+
+VAND_TEST(vand, i64)
+PVAND_TEST(pvand, i64)
