726884a9f2f2398a7469b4ddac7ca1038e2ac3b1
tkohara <tkohara-tgl@ncontr.com>
2020-12-03 10:40:15 +0900

    [VE] Add a regression test of vfmul and vfdiv intrinsic instructions

---
+++ llvm/test/CodeGen/VE/VELIntrinsics/c/vfmul.c
@@ -0,0 +1,67 @@
+#include "types.h"
+#include "velintrin.h"
+
+#define REGCALL fastcall
+
+/// Test vector floating multiply intrinsic instructions
+///
+/// Note:
+///   We test VFMUL*vvl, VFMUL*vvl_v, VFMUL*rvl, VFMUL*rvl_v, VFMUL*vvml_v,
+///   VFMUL*rvml_v, PVFMUL*vvl, PVFMUL*vvl_v, PVFMUL*rvl, PVFMUL*rvl_v,
+///   PVFMUL*vvml_v, and PVFMUL*rvml_v instructions.
+
+#define VFMUL_TEST(INST, TYPE) \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvl(__vr l, __vr r) { \
+  return _vel_ ## INST ## _vvvl(l, r, 256); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvvl(__vr l, __vr r, __vr b) { \
+  return _vel_ ## INST ## _vvvvl(l, r, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvl(TYPE l, __vr r) { \
+  return _vel_ ## INST ## _vsvl(l, r, 256); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvvl(TYPE l, __vr r, __vr b) { \
+  return _vel_ ## INST ## _vsvvl(l, r, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvmvl(__vr l, __vr r, __vm256 m, __vr b) { \
+  return _vel_ ## INST ## _vvvmvl(l, r, m, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvmvl(TYPE l, __vr r, __vm256 m, __vr b) { \
+  return _vel_ ## INST ## _vsvmvl(l, r, m, b, 128); \
+}
+
+#define PVFMUL_TEST(INST, TYPE) \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvl(__vr l, __vr r) { \
+  return _vel_ ## INST ## _vvvl(l, r, 256); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvvl(__vr l, __vr r, __vr b) { \
+  return _vel_ ## INST ## _vvvvl(l, r, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvl(TYPE l, __vr r) { \
+  return _vel_ ## INST ## _vsvl(l, r, 256); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvvl(TYPE l, __vr r, __vr b) { \
+  return _vel_ ## INST ## _vsvvl(l, r, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vvvMvl(__vr l, __vr r, __vm512 m, __vr b) { \
+  return _vel_ ## INST ## _vvvMvl(l, r, m, b, 128); \
+} \
+__attribute__ ((REGCALL)) \
+__vr INST ## _vsvMvl(TYPE l, __vr r, __vm512 m, __vr b) { \
+  return _vel_ ## INST ## _vsvMvl(l, r, m, b, 128); \
+}
+
+VFMUL_TEST(vfmuld, quad)
+VFMUL_TEST(vfmuls, double)
+PVFMUL_TEST(pvfmul, i64)
