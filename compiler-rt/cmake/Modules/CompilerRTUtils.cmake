0c13c54c9601a9f67ef421240fadd3919ab3c743
Simon Moll <simon.moll@emea.nec.com>
2020-12-08 11:27:01 +0100

    [VE] Don't try to link an executable in compiler-rt tests.. we are here to get this support

--- compiler-rt/cmake/Modules/CompilerRTUtils.cmake
+++ compiler-rt/cmake/Modules/CompilerRTUtils.cmake
@@ -1,5 +1,6 @@
 include(CMakePushCheckState)
 include(CheckSymbolExists)
+include(BuiltinTests)
 
 # Because compiler-rt spends a lot of time setting up custom compile flags,
 # define a handy helper function for it. The compile flags setting in CMake
