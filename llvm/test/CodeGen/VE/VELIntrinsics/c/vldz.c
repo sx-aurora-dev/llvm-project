6a9790cf8999b6f68adfa222b768b46399b210aa
Akira Maruoka <akmaru0266@gmail.com>
2021-03-13 06:24:27 +0900

    C tests for vpcnt, vbrv, vldz

---
+++ llvm/test/CodeGen/VE/VELIntrinsics/c/vldz.c
@@ -0,0 +1,42 @@
+#include "types.h"
+#include "velintrin.h"
+
+#define REGCALL fastcall
+
+/// Test vector leading zero count intrinsic instructions
+///
+/// Note:
+///   We test VLDZ*vl, VLDZ*vl_v, VLDZ*vml_v, PVLDZ*vl, PVLDZ*vl_v, PVLDZ*vml_v instructions.
+
+#define VLDZ_TEST(INST)  \
+__attribute__((REGCALL)) \
+__vr INST##_vvl(__vr v) { \
+  return _vel_##INST##_vvl(v, 256); \
+} \
+__attribute__((REGCALL)) \
+__vr INST##_vvvl(__vr v, __vr b) { \
+  return _vel_##INST##_vvvl(l, b, 128); \
+} \
+__attribute__((REGCALL)) \
+__vr INST##_vvmvl(__vr v, __vm256 m, __vr b) { \
+  return _vel_##INST##_vvmvl(l, m, b, 128); \
+}
+
+#define PVLDZ_TEST(INST) \
+__attribute__((REGCALL)) \
+__vr INST##_vvl(__vr v) { \
+  return _vel_##INST##_vvl(v, 256); \
+} \
+__attribute__((REGCALL)) \
+__vr INST##_vvvl(__vr v, __vr b) { \
+  return _vel_##INST##_vvvl(l, b, 128); \
+} \
+__attribute__((REGCALL)) \
+__vr INST##_vvMvl(__vr v, __vm512 m, __vr b) { \
+  return _vel_##INST##_vvMvl(l, m, b, 128); \
+}
+
+VLDZ_TEST(vldz)
+VLDZ_TEST(pvldzlo)
+VLDZ_TEST(pvldzup)
+PVLDZ_TEST(pvldz)
