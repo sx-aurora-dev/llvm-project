4f0cad1d0c1d3efaca8f18e298746a8506f407a5
Simon Moll <simon.moll@emea.nec.com>
2021-11-22 14:11:54 +0100

    [llvm-ve-vars] fix script path (when sourced), fixed CPATH

--- clang/tools/vars-script/llvmvevars.sh
+++ clang/tools/vars-script/llvmvevars.sh
@@ -1,6 +1,6 @@
 # !/usr/bin/env bash
 
-SELF_PATH=$(dirname $(readlink -f $0))
+SELF_PATH=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 PREFIX=$(readlink -m ${SELF_PATH}/..)
 LIB_PATH=${PREFIX}/lib
 INC_PATH=${PREFIX}/include
@@ -10,5 +10,5 @@ CMAKE_PREFIX=${PREFIX}/lib/cmake
 
 export PATH=${BIN_PATH}:${PATH}
 export LD_LIBRARY_PATH=${LIB_PATH}:${LD_LIBRARY_PATH}
-export CPATH=${INC_PATH}:${LD_LIBRARYPATH}
+export CPATH=${INC_PATH}:${CPATH}
 export CMAKE_MODULE_PATH=${CMAKE_PREFIX}/clang:${CMAKE_PREFIX}/llvm:${CMAKE_MODULE_PATH}
