6c510cbf7e17baa380bf8a181c3b43145fd50980
Kazushi (Jam) Marukawa <marukawa@nec.com>
2022-07-02 17:53:17 +0900

    [VE] Add load/store stack tests

---
+++ llvm/test/CodeGen/VE/Scalar/c/load_stk.c
@@ -0,0 +1,182 @@
+#include "types.h"
+#include "velintrin.h"
+#include "alloca.h"
+
+#define REGCALL fastcall
+
+/// Test store instructions
+///
+/// Note:
+///   We test store instructions using general stack, stack with dynamic
+///   allocation, stack with dynamic allocation and alignment, and stack
+///   with dynamic allocation, alignment, and spill.
+///
+/// Fist test using a stack for leaf function.
+///
+///   |                                              | Higher address
+///   |----------------------------------------------| <- old sp
+///   | Local variables of fixed size                |
+///   |----------------------------------------------| <- sp
+///   |                                              | Lower address
+///
+/// Access local variable using sp (%s11).  In addition, please remember
+/// that stack is aligned by 16 bytes.
+///
+/// Second test using a general stack.
+///
+///   |                                              | Higher address
+///   |----------------------------------------------|
+///   | Parameter area for this function             |
+///   |----------------------------------------------|
+///   | Register save area (RSA) for this function   |
+///   |----------------------------------------------|
+///   | Return address for this function             |
+///   |----------------------------------------------|
+///   | Frame pointer for this function              |
+///   |----------------------------------------------| <- fp(=old sp)
+///   | Local variables of fixed size                |
+///   |----------------------------------------------|
+///   |.variable-sized.local.variables.(VLAs)........|
+///   |..............................................|
+///   |..............................................|
+///   |----------------------------------------------| <- returned by alloca
+///   | Parameter area for callee                    |
+///   |----------------------------------------------|
+///   | Register save area (RSA) for callee          |
+///   |----------------------------------------------|
+///   | Return address for callee                    |
+///   |----------------------------------------------|
+///   | Frame pointer for callee                     |
+///   |----------------------------------------------| <- sp
+///   |                                              | Lower address
+///
+/// Access local variable using fp (%s9) since the size of VLA is not
+/// known.  At the beginning of the functions, allocates 240 + data
+/// bytes.  240 means RSA+RA+FP (=176) + Parameter (=64).
+///
+/// Third test using a general stack.
+///
+///   |                                              | Higher address
+///   |----------------------------------------------|
+///   | Parameter area for this function             |
+///   |----------------------------------------------|
+///   | Register save area (RSA) for this function   |
+///   |----------------------------------------------|
+///   | Return address for this function             |
+///   |----------------------------------------------|
+///   | Frame pointer for this function              |
+///   |----------------------------------------------| <- fp(=old sp)
+///   |.empty.space.to.make.part.below.aligned.in....|
+///   |.case.it.needs.more.than.the.standard.16-byte.| (size of this area is
+///   |.alignment....................................|  unknown at compile time)
+///   |----------------------------------------------|
+///   | Local variables of fixed size including spill|
+///   | slots                                        |
+///   |----------------------------------------------| <- bp(not defined by ABI,
+///   |.variable-sized.local.variables.(VLAs)........|       LLVM chooses SX17)
+///   |..............................................| (size of this area is
+///   |..............................................|  unknown at compile time)
+///   |----------------------------------------------| <- stack top (returned by
+///   | Parameter area for callee                    |               alloca)
+///   |----------------------------------------------|
+///   | Register save area (RSA) for callee          |
+///   |----------------------------------------------|
+///   | Return address for callee                    |
+///   |----------------------------------------------|
+///   | Frame pointer for callee                     |
+///   |----------------------------------------------| <- sp
+///   |                                              | Lower address
+///
+/// Access local variable using bp (%s17) since the size of alignment
+/// and VLA are not known.  At the beginning of the functions, allocates
+/// pad(240 + data + align) bytes.  Then, access data through bp + pad(240)
+/// since this address doesn't change even if VLA is dynamically allocated.
+///
+/// Fourth test using a general stack with some spills.
+///
+
+#define STK_TEST(KIND, TYPE) \
+__attribute__ ((REGCALL)) \
+TYPE KIND ## TYPE ## _stk() { \
+  volatile TYPE stk; \
+  TYPE v = stk; \
+  return v; \
+} \
+__attribute__ ((REGCALL)) \
+TYPE KIND ## TYPE ## _stk_big() { \
+  volatile TYPE stk; \
+  volatile TYPE array[268435455]; \
+  TYPE v, v2; \
+  v = stk; \
+  for (i64 i = 0; i < 268435455; ++i) { \
+    v2 = array[i]; \
+  } \
+  return v; \
+} \
+__attribute__ ((REGCALL)) \
+TYPE KIND ## TYPE ## _stk_big2() { \
+  volatile TYPE stk; \
+  volatile TYPE array[268435456]; \
+  TYPE v, v2; \
+  v = stk; \
+  for (i64 i = 0; i < 268435456; ++i) { \
+    v2 = array[i]; \
+  } \
+  return v; \
+}
+
+#define DYN_TEST(KIND, TYPE) \
+__attribute__ ((REGCALL)) \
+TYPE KIND ## TYPE ## _stk_dyn(i64 bytes) { \
+  volatile TYPE stk; \
+  volatile TYPE* ptr = __builtin_alloca_with_align(bytes, 32); \
+  TYPE v, v2; \
+  v2 = *ptr; \
+  v = stk; \
+  return v; \
+}
+
+#define DYN_ALIGN_TEST(KIND, TYPE) \
+__attribute__ ((REGCALL)) \
+TYPE KIND ## TYPE ## _stk_dyn_align(i64 bytes) { \
+  volatile struct { TYPE stk; } s __attribute__ ((aligned (32))); \
+  volatile TYPE* ptr = __builtin_alloca_with_align(bytes, 32); \
+  TYPE v, v2; \
+  v2 = *ptr; \
+  v = s.stk; \
+  return v; \
+}
+
+#define DYN_ALIGN2_TEST(KIND, TYPE) \
+__attribute__ ((REGCALL)) \
+TYPE KIND ## TYPE ## _stk_dyn_align2(i64 bytes) { \
+  volatile struct { TYPE stk; } s __attribute__ ((aligned (32))); \
+  volatile TYPE* ptr = __builtin_alloca_with_align(bytes, 32); \
+  TYPE v, v2, v3; \
+  v2 = *ptr; \
+  v = s.stk; \
+  volatile struct { TYPE stk; } s2 __attribute__ ((aligned (64))); \
+  v3 = s2.stk; \
+  return v; \
+}
+
+#define DYN_ALIGN_SPILL_TEST(KIND, TYPE) \
+__attribute__ ((REGCALL)) \
+TYPE KIND ## TYPE ## _stk_dyn_align_spill(i64 bytes) { \
+  volatile struct { TYPE stk; } s __attribute__ ((aligned (32))); \
+  volatile TYPE* ptr = __builtin_alloca_with_align(bytes, 32); \
+  extern void dummy(); \
+  extern void pass(i64); \
+  TYPE v, v2; \
+  v2 = *ptr; \
+  v = s.stk; \
+  dummy(); \
+  pass(bytes); \
+  return v; \
+}
+
+STK_TEST(load, i64)
+DYN_TEST(load, i64)
+DYN_ALIGN_TEST(load, i64)
+DYN_ALIGN2_TEST(load, i64)
+DYN_ALIGN_SPILL_TEST(load, i64)
