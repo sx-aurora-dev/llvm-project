7e87920f57567336f2abc3817eddc4486f124527
Kazushi (Jam) Marukawa <marukawa@nec.com>
2022-09-05 21:25:33 +0900

    [VE] Update fp_trunc test
    Update fp_trunc tests as a step to update whole fp tests.

--- llvm/test/CodeGen/VE/Scalar/c/fp_trunc.c
+++ llvm/test/CodeGen/VE/Scalar/c/fp_trunc.c
@@ -1,70 +1,25 @@
-#include <math.h>
-typedef _Bool int1_t;
-typedef char int8_t;
-typedef unsigned char uint8_t;
-typedef short int16_t;
-typedef unsigned short uint16_t;
-typedef int int32_t;
-typedef unsigned int uint32_t;
-typedef long int64_t;
-typedef unsigned long uint64_t;
-typedef __int128 int128_t;
-typedef unsigned __int128 uint128_t;
-typedef long double quad;
-typedef _Complex float fcomp;
-typedef _Complex double dcomp;
-typedef _Complex long double qcomp;
+#include "types.h"
 
-#define FP_TRUNCF_VAR(TY) \
-TY func_fp_truncf_var_ ## TY(TY a) { \
-  return truncf(a); \
-}
-FP_TRUNCF_VAR(float)
-
-#define FP_TRUNC_VAR(TY) \
+#define FP_TRUNC_VAR(TY, NAME) \
 TY func_fp_trunc_var_ ## TY(TY a) { \
-  return trunc(a); \
+  return __builtin_ ## NAME(a); \
 }
-FP_TRUNC_VAR(double)
+FP_TRUNC_VAR(float, truncf)
+FP_TRUNC_VAR(double, trunc)
+FP_TRUNC_VAR(quad, truncl)
 
-#define FP_TRUNCL_VAR(TY) \
-TY func_fp_truncl_var_ ## TY(TY a) { \
-  return truncl(a); \
+#define FP_TRUNC_ZERO(TY, NAME) \
+TY func_fp_trunc_zero_ ## TY() { \
+  return __builtin_ ## NAME((TY)0.0); \
 }
-FP_TRUNCL_VAR(quad)
+FP_TRUNC_ZERO(float, truncf)
+FP_TRUNC_ZERO(double, trunc)
+FP_TRUNC_ZERO(quad, truncl)
 
-#define FP_TRUNCF_ZERO(TY) \
-TY func_fp_truncf_zero_ ## TY() { \
-  return  truncf((TY)0.0) ; \
-}
-FP_TRUNCF_ZERO(float)
-
-#define FP_TRUNC_ZERO(TY) \
-TY func_fp_TRUNC_zero_ ## TY() { \
-  return  trunc((TY)0.0) ; \
-}
-FP_TRUNC_ZERO(double)
-
-#define FP_TRUNCL_ZERO(TY) \
-TY func_fp_truncl_zero_ ## TY() { \
-  return  truncl((TY)0.0) ; \
-}
-FP_TRUNCL_ZERO(quad)
-
-#define FP_TRUNCF_CONST(TY) \
-TY func_fp_truncf_const_ ## TY() { \
-  return truncf((TY)-2.0); \
-}
-FP_TRUNCF_CONST(float)
-
-#define FP_TRUNC_CONST(TY) \
+#define FP_TRUNC_CONST(TY, NAME) \
 TY func_fp_trunc_const_ ## TY() { \
-  return trunc((TY)-2.0); \
+  return __builtin_ ## NAME((TY)-2.0); \
 }
-FP_TRUNC_CONST(double)
-
-#define FP_TRUNCL_CONST(TY) \
-TY func_fp_truncl_const_ ## TY() { \
-  return truncl((TY)-2.0); \
-}
-FP_TRUNCL_CONST(quad)
+FP_TRUNC_CONST(float, truncf)
+FP_TRUNC_CONST(double, trunc)
+FP_TRUNC_CONST(quad, truncl)
