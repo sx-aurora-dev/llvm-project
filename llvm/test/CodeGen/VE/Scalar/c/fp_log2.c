f92891e2f468769a7390cc46f04dba6f3f0a07ca
Kazushi (Jam) Marukawa <marukawa@nec.com>
2022-09-05 20:40:51 +0900

    [VE] Update fp_log2 test
    Update fp_log2 tests as a step to update whole fp tests.

--- llvm/test/CodeGen/VE/Scalar/c/fp_log2.c
+++ llvm/test/CodeGen/VE/Scalar/c/fp_log2.c
@@ -1,70 +1,25 @@
-#include <math.h>
-typedef _Bool int1_t;
-typedef char int8_t;
-typedef unsigned char uint8_t;
-typedef short int16_t;
-typedef unsigned short uint16_t;
-typedef int int32_t;
-typedef unsigned int uint32_t;
-typedef long int64_t;
-typedef unsigned long uint64_t;
-typedef __int128 int128_t;
-typedef unsigned __int128 uint128_t;
-typedef long double quad;
-typedef _Complex float fcomp;
-typedef _Complex double dcomp;
-typedef _Complex long double qcomp;
+#include "types.h"
 
-#define FP_LOG2F_VAR(TY) \
-TY func_fp_log2f_var_ ## TY(TY a) { \
-  return log2f(a); \
-}
-FP_LOG2F_VAR(float)
-
-#define FP_LOG2_VAR(TY) \
+#define FP_LOG2_VAR(TY, NAME) \
 TY func_fp_log2_var_ ## TY(TY a) { \
-  return log2(a); \
+  return __builtin_ ## NAME(a); \
 }
-FP_LOG2_VAR(double)
+FP_LOG2_VAR(float, log2f)
+FP_LOG2_VAR(double, log2)
+FP_LOG2_VAR(quad, log2l)
 
-#define FP_LOG2L_VAR(TY) \
-TY func_fp_log2l_var_ ## TY(TY a) { \
-  return log2l(a); \
+#define FP_LOG2_ZERO(TY, NAME) \
+TY func_fp_log2_zero_ ## TY() { \
+  return __builtin_ ## NAME((TY)0.0); \
 }
-FP_LOG2L_VAR(quad)
+FP_LOG2_ZERO(float, log2f)
+FP_LOG2_ZERO(double, log2)
+FP_LOG2_ZERO(quad, log2l)
 
-#define FP_LOG2F_ZERO(TY) \
-TY func_fp_log2f_zero_ ## TY() { \
-  return  log2f((TY)0.0) ; \
-}
-FP_LOG2F_ZERO(float)
-
-#define FP_LOG2_ZERO(TY) \
-TY func_fp_LOG2_zero_ ## TY() { \
-  return  log2((TY)0.0) ; \
-}
-FP_LOG2_ZERO(double)
-
-#define FP_LOG2L_ZERO(TY) \
-TY func_fp_log2l_zero_ ## TY() { \
-  return  log2l((TY)0.0) ; \
-}
-FP_LOG2L_ZERO(quad)
-
-#define FP_LOG2F_CONST(TY) \
-TY func_fp_log2f_const_ ## TY() { \
-  return log2f((TY)-2.0); \
-}
-FP_LOG2F_CONST(float)
-
-#define FP_LOG2_CONST(TY) \
+#define FP_LOG2_CONST(TY, NAME) \
 TY func_fp_log2_const_ ## TY() { \
-  return log2((TY)-2.0); \
+  return __builtin_ ## NAME((TY)-2.0); \
 }
-FP_LOG2_CONST(double)
-
-#define FP_LOG2L_CONST(TY) \
-TY func_fp_log2l_const_ ## TY() { \
-  return log2l((TY)-2.0); \
-}
-FP_LOG2L_CONST(quad)
+FP_LOG2_CONST(float, log2f)
+FP_LOG2_CONST(double, log2)
+FP_LOG2_CONST(quad, log2l)
