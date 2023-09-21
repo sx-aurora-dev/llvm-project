45e738ffe86490eb0b191a9b21cccf2278efa554
Kazushi (Jam) Marukawa <marukawa@nec.com>
2023-09-21 11:14:42 +0900

    [VE] Solve merge conflict

--- clang/lib/Driver/ToolChains/VEToolchain.cpp
+++ clang/lib/Driver/ToolChains/VEToolchain.cpp
@@ -147,9 +147,10 @@ void VEToolChain::AddCXXStdlibLibArgs(const ArgList &Args,
   CmdArgs.push_back(Args.MakeArgString(getDriver().SysRoot + "/opt/nec/ve/lib"));
 
   // Add libc++.so rpath.
-  CmdArgs.push_back("-rpath");
-  for (auto &Path : getStdlibPaths())
-    CmdArgs.push_back(Args.MakeArgString(std::move(Path)));
+  if (std::optional<std::string> Path = getStdlibPath()) {
+    CmdArgs.push_back("-rpath");
+    CmdArgs.push_back(Args.MakeArgString(*Path));
+  }
 
   CmdArgs.push_back("-lc++");
   if (Args.hasArg(options::OPT_fexperimental_library))
